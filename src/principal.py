c8a68add34f9225c08d8d747c728e8259b1a6e35
adrian <adrian11_12@hotmail.com>
2013-07-24 23:27:20 -0500

    Todas las conexiones realizadas
    se crearon las conexiones entre las ventanas y las conexiones de los
    botones con sus funciones

--- src/principal.py
+++ src/principal.py
@@ -1,13 +1,24 @@
+'''
+Created on 24/07/2013
+
+@author: xtreme
+'''
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'principal.ui'
 #
-# Created: Sun Jul 21 14:41:16 2013
+# Created: Fri Jul 19 08:41:26 2013
 #      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt4 import QtCore, QtGui
+from qtablero import Ui_nivel
+import sys
+#import images 
+
+
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -23,12 +34,22 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_principal(object):
+class Ui_principal(QtGui.QMainWindow):
     def setupUi(self, principal):
         principal.setObjectName(_fromUtf8("principal"))
         principal.resize(400, 300)
+        self.imagen = QtGui.QPushButton(principal)
+        self.imagen.setGeometry(QtCore.QRect(-6, -8, 411, 311))
+        self.imagen.setText(_fromUtf8(""))
+        icon = QtGui.QIcon("./images/ININ.png")
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("./images/ININ.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.imagen.setIcon(icon)
+        self.imagen.setIconSize(QtCore.QSize(400, 300))
+        self.imagen.setObjectName(_fromUtf8("imagen"))
         self.pushButton = QtGui.QPushButton(principal)
         self.pushButton.setGeometry(QtCore.QRect(30, 20, 91, 51))
+        
+        
         font = QtGui.QFont()
         font.setPointSize(15)
         font.setBold(True)
@@ -48,12 +69,14 @@ class Ui_principal(object):
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.pushButton_3 = QtGui.QPushButton(principal)
         self.pushButton_3.setGeometry(QtCore.QRect(270, 240, 121, 41))
+    
         font = QtGui.QFont()
         font.setPointSize(15)
         font.setBold(True)
         font.setWeight(75)
         self.pushButton_3.setFont(font)
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        
         self.pushButton_4 = QtGui.QPushButton(principal)
         self.pushButton_4.setGeometry(QtCore.QRect(10, 150, 141, 41))
         font = QtGui.QFont()
@@ -62,18 +85,23 @@ class Ui_principal(object):
         font.setWeight(75)
         self.pushButton_4.setFont(font)
         self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
-        self.imagen = QtGui.QPushButton(principal)
-        self.imagen.setGeometry(QtCore.QRect(-6, -8, 411, 311))
-        self.imagen.setText(_fromUtf8(""))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/images/ININ.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.imagen.setIcon(icon)
-        self.imagen.setIconSize(QtCore.QSize(400, 300))
-        self.imagen.setObjectName(_fromUtf8("imagen"))
-
         self.retranslateUi(principal)
         QtCore.QMetaObject.connectSlotsByName(principal)
-
+        self.connect(self.pushButton_3,QtCore.SIGNAL('clicked()'),QtGui.qApp, QtCore.SLOT('quit()'))
+        self.connect(self.pushButton, QtCore.SIGNAL("clicked()"), self.abrirEntrar)
+        #self.connect(self.pushButton, QtCore.SIGNAL("clicked()"), self.close)
+        
+        
+     
+        
+    def abrirEntrar(self):
+        nivel = QtGui.QMainWindow()
+        ui1 = Ui_nivel()
+        ui1.setupUi(nivel)
+        principal.setVisible(False)
+        nivel.show()
+        ui1.exec_() 
+        
     def retranslateUi(self, principal):
         principal.setWindowTitle(_translate("principal", "sudoku", None))
         self.pushButton.setText(_translate("principal", "Jugar", None))
@@ -81,12 +109,9 @@ class Ui_principal(object):
         self.pushButton_3.setText(_translate("principal", "Salir", None))
         self.pushButton_4.setText(_translate("principal", "Mejores Puntajes", None))
 
-import images_rc
-
 if __name__ == "__main__":
-    import sys
     app = QtGui.QApplication(sys.argv)
-    principal = QtGui.QDialog()
+    principal = QtGui.QMainWindow()
     ui = Ui_principal()
     ui.setupUi(principal)
     principal.show()
--- src/qtablero.py
+++ src/qtablero.py
@@ -1,3 +1,8 @@
+'''
+Created on 24/07/2013
+
+@author: xtreme
+'''
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'qtablero.ui'
@@ -7,11 +12,13 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt4 import QtCore, QtGui
 from tkinter import *
 import random
 from copy import deepcopy
 import time
+import sys
 
 
 try:
@@ -28,16 +35,15 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_QTablero(object):
-
-
-
-
-
+class Ui_QTablero(QtGui.QMainWindow):
+    nombre="hola_q_hace"
+    dif=1
+   
     def setupUi(self, QTablero):
         QTablero.setObjectName(_fromUtf8("QTablero"))
         QTablero.resize(750, 522)
         QTablero.setAcceptDrops(True)
+        
         self.centralWidget = QtGui.QWidget(QTablero)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
         self.gridLayoutWidget = QtGui.QWidget(self.centralWidget)
@@ -213,83 +219,59 @@ class Ui_QTablero(object):
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionExit)
         self.menuBar.addAction(self.menuFile.menuAction())
-        self.partidanueva(1)
-        self.retranslateUi(QTablero)
-        QtCore.QMetaObject.connectSlotsByName(QTablero)
-        self.ll= QtGui.QLineEdit()
-
-
-
-
-    def partidanueva(self, d):
-        self.a =33
-        if d==1:
-          self.a=33
-        if d==2:
-          self.a=25
-        if d==3:
-          self.a=19
-
-
-        self.gridLayout_2.addWidget(QtGui.QLabel("Jugador:"),1,0)
-        self.gridLayout_2.addWidget(QtGui.QLabel("jugado"),1,1)
-        self.gridLayout_2.addWidget(QtGui.QLabel("Puntaje:"),2,0)
-        self.gridLayout_2.addWidget(QtGui.QLabel("puntos"),2,1)
-
-#carga el archivo
         f=open("ola k ase.txt","r")
         i=0
-        tablero=[]
-        tablero_original=[]
-        habilitados=[]
-        tableroresuelto=[]
+        self.tablero=[]
+        self.tablero_original=[]
+        self.habilitados=[]
+        self.tableroresuelto=[]
         for line in f:
             linea=line.split(',')
-            tableroresuelto.append(linea)
+            self.tableroresuelto.append(linea)
             i=i+1
             print ("\n")
+        self.contpuntos="0"
+        self.tablero=deepcopy(self.tableroresuelto)
+        self.habilitados= deepcopy(self.tableroresuelto)
+        self.tablero_original= deepcopy(self.tableroresuelto)
+        self.verificarficha()
+        self.partidanueva(self.dif)
+        self.ayudaboton()
+        self.verificar_fila(6,4)
+        self.verificar_columna(5,4)
+        self.verificarsubcuadricula(5,3,6)
+        self.on_pushbutton_2_clicked()
+        #self.control_ingreso_ficha()
+        self.retranslateUi(QTablero)
+        self.connect(self.pushButton_2, QtCore.SIGNAL("clicked()"), self.on_pushbutton_2_clicked)
+        self.connect(self.pushButton, QtCore.SIGNAL("clicked()"), self.ayudaboton)
+        self.connect(self.pushButton_4, QtCore.SIGNAL("clicked()"), self.guardar)
+        QtCore.QMetaObject.connectSlotsByName(QTablero)
+        self.ll= QtGui.QLineEdit()
+        print(self.nombre)
+
+
+    def settablero(self,set):
         i=0
         j=0
-
-
-
-#ingresa 0 los numeros aleatoriamente
-
-        k=0
-        tablero=deepcopy(tableroresuelto)
-        habilitados= deepcopy(tableroresuelto)
-        tablero_original= deepcopy(tableroresuelto)
-
-        while k<self.a:
-            x = random.randrange(0, 9)
-            y= random.randrange(0, 9)
-            if int(tablero[x][y])!=0 :
-               habilitados[x][y]="0"
-               tablero[x][y]="0"
-            k=k+1
-
-
-#ingresa las fichas al tablero
-        ind=QtGui.QDoubleValidator(1, 9, 0, QTablero)
-
+        ind=QtGui.QDoubleValidator(1, 9, 0, self)
         while i<9:
           j=0
           while j<9:
-             if int( habilitados[i][j])!=0:
-                 habilitados[i][j]="1"
-             print ( habilitados[i][j])
-             print ( tablero_original[i][j])
-             print (tablero[i][j])
-             line=QtGui.QLineEdit(tablero[i][j])
+            if set==0:
+             if int( self.habilitados[i][j])!=0:
+                 self.habilitados[i][j]="1"
+             print ( self.habilitados[i][j])
+             print ( self.tablero_original[i][j])
+             print (self.tablero[i][j])
+             line=QtGui.QLineEdit(self.tablero[i][j])
 
-             if int(habilitados[i][j])==1:
+             if int(self.habilitados[i][j])==1:
                  line.setEnabled(False)
-             if int(tablero[i][j])==0:
+             if int(self.tablero[i][j])==0:
                  line.setText("")
                  line.setMaxLength(1)
                  line.setValidator(ind)
-
-
              if i>=0 and i<=2 and j>=0 and j<=2:
                 self.celda1.addWidget(line,i,j)
              if i>=0 and i<=2 and j>=3 and j<=5:
@@ -308,46 +290,105 @@ class Ui_QTablero(object):
                    self.celda8.addWidget(line,i,j)
              if i>=6 and i<=8 and j>=6 and j<=8:
                    self.celda9.addWidget(line,i,j)
-             j=j+1
+            #if set==1:
+             #   if i>=0 and i<=2 and j>=0 and j<=2:
+              #    self.celda1.removeWidget(line,i,j)
+               # if i>=0 and i<=2 and j>=3 and j<=5:
+                #    self.celda2.removeWidget(line,i,j)
+       #         if i>=0 and i<=2 and j>=6 and j<=8:
+        #           self.celda3.removeWidget(line,i,j)
+         #       if i>=3 and i<=5 and j>=0 and j<=2:
+          #         self.celda4.removeWidget(line,i,j)
+           #     if i>=3 and i<=5 and j>=3 and j<=5:
+            #       self.celda5.removeWidget(line,i,j)
+             #   if i>=3 and i<=5 and j>=6 and j<=8:
+              #     self.celda6.removeWidget(line,i,j)
+               # if i>=6 and i<=8 and j>=0 and j<=2:
+       #            self.celda7.removeWidget(line,i,j)
+        #        if i>=6 and i<=8 and j>=3 and j<=5:
+         #          self.celda8.removeWidget(line,i,j)
+          #      if i>=6 and i<=8 and j>=6 and j<=8:
+           #        self.celda9.removeWidget(line,i,j)
+
+
+            j=j+1
+
           i=i+1
 
+    def partidanueva(self, d):
+        self.a =0
+        if d==3:
+          self.a=69
+        if d==2:
+          self.a=56
+        if d==1:
+          self.a=38
+        
 
+        self.gridLayout_2.addWidget(QtGui.QLabel("Jugador:"),1,0)
+        self.gridLayout_2.addWidget(QtGui.QLabel(self.nombre),1,1)
+        self.gridLayout_2.addWidget(QtGui.QLabel("Puntaje:"),2,0)
+        self.gridLayout_2.addWidget(QtGui.QLabel(self.contpuntos),2,1)
 
+#carga el archivo
+
+        i=0
+        j=0
+
+#ingresa 0 los numeros aleatoriamente
+
+        k=0
+        while k<self.a:
+            print(self.a)
+            print(k)
+            x = random.randrange(0, 9)
+            y= random.randrange(0, 9)
+            if int(self.tablero[x][y])!=0 :
+                self.habilitados[x][y]="0"
+                self.tablero[x][y]="0"
+            k=k+1
+#ingresa las fichas al tablero
+        self.settablero(0)
 
 
     def verificar_fila(self,ficha,m):
+        print("entra de verificar fila") 
         num=0
         b=0
         j=0
         while j<9:
             if   int(self.tablero[j][m])!=0:
-              b=b+1
+                b=b+1
             if num>1:
                 return 0
             j=j+1
         #if b==9:
             ##aqui van los puntajes para q valla aumentando
+        print("sale de verificar fila")    
         return 1
 
 
-    def verificar_columna(self,B,l,m):
+    def verificar_columna(self,ficha,l):
+        print("entra de verificar col") 
         num=0
         b=0
         j=0
         while j<9:
             if int(self.tablero[l][j])!=0:
                b=b+1
+            if int(self.tablero[l][j])== ficha:
+               num=num+1
             j=j+1
-        if int(self.tablero[l][j])==B:
-          num=num+1
         if num>1:
             return 0
             #if b==9
             #aqui van las lineas para cambiar el puntaje
+        print("sale de verificar col") 
         return 1
 
 
-    def verificarsubcuadricula(ficha,l,m):
+    def verificarsubcuadricula(self,ficha,l,m):
+        print("entra de verificar sub") 
         if l<=2 and m<=2:
            b=0
            num=0
@@ -366,9 +407,6 @@ class Ui_QTablero(object):
              i=i+1
              if num>1:
                  return 0
-
-
-
 
         if l<=2 and m<=3 and m<=5:
            b=0
@@ -389,9 +427,6 @@ class Ui_QTablero(object):
              if num>1:
                  return 0
 
-
-
-
         if l<=2 and m<=6 and m<=8:
            b=0
            num=0
@@ -410,10 +445,6 @@ class Ui_QTablero(object):
              i=i+1
              if num>1:
                  return 0
-
-
-
-
 
         if l<=3 and l<=5  and m<=2:
            b=0
@@ -434,9 +465,6 @@ class Ui_QTablero(object):
              if num>1:
                  return 0
 
-
-
-
         if l<=3 and l<=5 and m<=3 and m<=5:
            b=0
            num=0
@@ -455,9 +483,6 @@ class Ui_QTablero(object):
              i=i+1
              if num>1:
                  return 0
-
-
-
 
 
         if l<=3 and l<=5 and m<=6 and m<=8:
@@ -480,8 +505,6 @@ class Ui_QTablero(object):
                  return 0
 
 
-
-
         if l<=6 and l<=8 and m<=2:
            b=0
            num=0
@@ -500,8 +523,6 @@ class Ui_QTablero(object):
              i=i+1
              if num>1:
                  return 0
-
-
 
 
         if l<=6 and l<=8 and m<=3 and m<=5:
@@ -524,7 +545,6 @@ class Ui_QTablero(object):
                  return 0
 
 
-
         if l<=6 and l<=8 and m<=6 and m<=8:
            b=0
            num=0
@@ -543,6 +563,7 @@ class Ui_QTablero(object):
              i=i+1
              if num>1:
                  return 0
+        print("sale de verificar sub") 
         return 1
 
 
@@ -558,48 +579,72 @@ class Ui_QTablero(object):
           i=i+1
         if b==0:
             ##aqui van los puntajes
-              QtGui.QMessageBox.information( QTablero,"sudoku message","Felicitaciones has terminado el sudoku",QtGui.QMessageBox.Ok)
+              QtGui.QMessageBox.information( self,"sudoku message","Hola k ase",QtGui.QMessageBox.Ok)
+         
 
-
-    def control_ingreso_ficha():
+    def control_ingreso_ficha(self):
         i=0
         j=0
+        
         while i<9:
-          while j<9:
-              if int( tablero_original[i][j])!= int( tablero[i][j]):
+            while j<9:
                 k=0
-                if  verificar_fila(int( tablero[i][j]),j)==0:
-                    QtGui.QMessageBox.information( QTablero,"sudoku message","número "+tablero[i][j]+" repetido en fila",QtGui.QMessageBox.Ok)
-                if verificar_fila(int( tablero[i][j]),j)!=0:
-                    k=k+1
-                if  verificar_columna(int( tablero[i][j]),i,j)==0:
-                    QtGui.QMessageBox.information( QTablero,"sudoku message","número "+tablero[i][j]+" repetido en columna",QtGui.QMessageBox.Ok)
-                if  verificar_columna(int( tablero[i][j]),i,j)!=0:
-                    k=k+1
-                if   verificarsubcuadricula(int( tablero[i][j]),i,j)==0:
-                    QtGui.QMessageBox.information( QTablero,"sudoku message","número "+tablero[i][j]+" repetido en subcuadricula",QtGui.QMessageBox.Ok)
-                if   verificarsubcuadricula(int( tablero[i][j]),i,j)==0:
-                    k=k+1
+                if int(self.tablero_original[i][j])!= int( self.tablero[i][j]):
+                   
+                    if  self.verificar_fila(int( self.tablero[i][j]),j)==0:
+                        if int( self.tablero[i][j])!=0:
+                            QtGui.QMessageBox.information( QTablero,"sudoku message","número repetido en fila",QtGui.QMessageBox.Ok)
+                    if self.verificar_fila(int( self.tablero[i][j]),j)!=0:
+                        k=k+1
+                    if  self.verificar_columna(int( self.tablero[i][j]),i)==0:
+                        if int( self.tablero[i][j])!=0:
+                             QtGui.QMessageBox.information( QTablero,"sudoku message","número repetido en columna",QtGui.QMessageBox.Ok)
+                    if  self.verificar_columna(int( self.tablero[i][j]),i)!=0:
+                        k=k+1
+                    if   self.verificarsubcuadricula(int( self.tablero[i][j]),i,j)==0:
+                        if int( self.tablero[i][j])!=0:
+                            QtGui.QMessageBox.information( QTablero,"sudoku message","número repetido en subcuadricula",QtGui.QMessageBox.Ok)
+                    if   self.verificarsubcuadricula(int( self.tablero[i][j]),i,j)==0:
+                        k=k+1
                 if k==3:
-                   tablero_original[i][j]= tablero[i][j]
-                   #aqui debe ir aumentando puntaje y seteando
+                    self.tablero_original[i][j]= self.tablero[i][j]
+                    #aqui debe ir aumentando puntaje y seteando
+                j=j+1
+            i=i+1
 
 
-    def ayudaboton():
-      i=0
-      while i<1:
-        x = random.randrange(0, 9)
-        y= random.randrange(0, 9)
-        if int(tablero[x][y])!=0 and int(habilitados[x][y])==0:
-             tablero[x][y]=tablero_original[x][y]
-        if int(tablero[x][y])==0 or int(habilitados[x][y])==1:
-           i=i+1
+
+
+    def on_pushbutton_2_clicked(self):
+        self.control_ingreso_ficha()
+        self.verificarficha()
+        print ("hola")
+
+
+
+
+    def ayudaboton(self):
+        
+        i=0
+        while i<1:
+            x = random.randrange(0, 9)
+            y= random.randrange(0, 9)
+            print(self.tablero[x][y])
+            if int(self.tablero[x][y])!=0 :
+                print ("hla ayuda4", self.tablero[x][y],x,y)
+                i=i-1
+            if int(self.tablero[x][y])==0 :
+                self.tablero[x][y]=self.tableroresuelto[x][y]
+                # self.ayudaudaboton(1)
+                print ("hla ayuda",self.tablero[x][y],x,y)
+                i=1
+
 
 
 
     def retranslateUi(self, QTablero):
         QTablero.setWindowTitle(_translate("QTablero", "Sudoku", None))
-        self.label.setText(_translate("QTablero", "Verificar Número Ingresado", None))
+        self.label.setText(_translate("QTablero", "Verificar Numero Ingresado", None))
         self.label_2.setText(_translate("QTablero", "Ayuda ", None))
         self.label_3.setText(_translate("QTablero", "Guardar Partida", None))
         self.label_4.setText(_translate("QTablero", "Salir", None))
@@ -607,10 +652,82 @@ class Ui_QTablero(object):
         self.actionQuit_2.setText(_translate("QTablero", "Quit", None))
         self.actionExit.setText(_translate("QTablero", "Exit", None))
 
+    def guardar(self):
+        QtGui.QMessageBox.information( self,"sudoku message","Su juego se ha guardado exitosamente",QtGui.QMessageBox.Ok)
+        
+
+
+class Ui_nivel(QtGui.QMainWindow):
+    def setupUi(self, nivel):
+        nivel.setObjectName(_fromUtf8("nivel"))
+        nivel.resize(400, 295)
+        self.pushButton = QtGui.QPushButton(nivel)
+        self.pushButton.setGeometry(QtCore.QRect(50, 210, 60, 60))
+        self.pushButton.setText(_fromUtf8(""))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("./images/visto.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(60, 60))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(nivel)
+        self.pushButton_2.setGeometry(QtCore.QRect(300, 210, 60, 60))
+        self.pushButton_2.setText(_fromUtf8(""))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("./images/x.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_2.setIcon(icon1)
+        self.pushButton_2.setIconSize(QtCore.QSize(60, 60))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.facil = QtGui.QRadioButton(nivel)
+        self.facil.setGeometry(QtCore.QRect(200, 120, 82, 17))
+        self.facil.setChecked(True)
+        self.facil.setObjectName(_fromUtf8("facil"))
+        self.medio = QtGui.QRadioButton(nivel)
+        self.medio.setGeometry(QtCore.QRect(200, 150, 82, 17))
+        self.medio.setObjectName(_fromUtf8("medio"))
+        self.dificil = QtGui.QRadioButton(nivel)
+        self.dificil.setGeometry(QtCore.QRect(200, 180, 82, 17))
+        self.dificil.setObjectName(_fromUtf8("dificil"))
+        self.label = QtGui.QLabel(nivel)
+        self.label.setGeometry(QtCore.QRect(60, 60, 71, 21))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.nombretexto = QtGui.QLineEdit(nivel)
+        self.nombretexto.setGeometry(QtCore.QRect(160, 60, 191, 20))
+        self.nombretexto.setObjectName(_fromUtf8("nombretexto"))
+        self.label_2 = QtGui.QLabel(nivel)
+        self.label_2.setGeometry(QtCore.QRect(60, 122, 101, 21))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.connect(self.pushButton, QtCore.SIGNAL("clicked()"), self.abrirEntrar) 
+        self.retranslateUi(nivel)
+        QtCore.QMetaObject.connectSlotsByName(nivel)
+        
+    def abrirEntrar(self):
+        self.d=1
+        if self.facil.isChecked():
+            self.d=1
+        if self.medio.isChecked():
+            self.d=2
+        if self.dificil.isChecked():
+            self. d=3
+        qtablero = QtGui.QMainWindow()
+        ui1 = Ui_QTablero()
+        ui1.dif=self.d 
+        ui1.nombre=self.nombretexto.text()
+        ui1.setupUi(qtablero)
+        self.setVisible(False)
+        qtablero.show()
+        ui1.exec_() 
+
+    def retranslateUi(self, nivel):
+        nivel.setWindowTitle(_translate("nivel", "Juego Nuevo", None))
+        self.facil.setText(_translate("nivel", "facil", None))
+        self.medio.setText(_translate("nivel", "medio", None))
+        self.dificil.setText(_translate("nivel", "dificikl", None))
+        self.label.setText(_translate("nivel", "<html><head/><body><p><span style=\" font-size:12pt;\">Jugador:</span></p></body></html>", None))
+        self.nombretexto.setText(_translate("nivel", "nombre del jugador", None))
+        self.label_2.setText(_translate("nivel", "<html><head/><body><p><span style=\" font-size:12pt;\">Dificultad</span></p></body></html>", None))
 
 
 if __name__ == "__main__":
-    import sys
     app = QtGui.QApplication(sys.argv)
     QTablero = QtGui.QMainWindow()
     ui = Ui_QTablero()
